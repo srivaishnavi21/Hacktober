4396231ba837eb7b8d58867ed7b808c543fb3816
SatyaShodhaka <satyashodhaka66@gmail.com>
2019-10-25 20:45:46 +0530

    hell

--- uzi/uzi/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect
-from django.shortcuts import render,HttpResponse,redirect 
-
-def login_redirect(request):
-    return redirect('/account/profile')
-
-def about(request):
-    return render(request,'accounts/about.html')
