383bf91c7cd58446b61f5044041f6c850114616e
SatyaShodhaka <satyashodhaka66@gmail.com>
2019-10-25 20:50:38 +0530

    hell

---
+++ uzi/accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=50,default='')
+    website = models.URLField(default='')
+    image = models.ImageField(upload_to='accounts/',default='accounts/user.png',blank = True)
+    def __str__(self):
+        return self.user.username
+        
+#creating the userprofile for the superusers
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.get_or_create( user=kwargs['instance'])
+
+post_save.connect( create_profile ,sender=User)
